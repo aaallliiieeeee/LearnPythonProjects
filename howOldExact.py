7c5956bbfd8c03157dd6b48d09e4e4da961128b4
aaallliiieeeee <32971113+aaallliiieeeee@users.noreply.github.com>
2017-11-06 17:16:44 -0500

    Add files via upload
    Breaks down your age to the second

---
+++ howOldExact.py
@@ -0,0 +1,7 @@
+print("Let's see how long you have lived in days, minutes, and seconds.")
+print("Please enter your age...")
+age = int(input("Age: "))
+days = age * 356
+minutes = age * 525948
+seconds = age * 31556926
+print 'You have been alive for', days, 'days', minutes, 'minutes and', seconds, 'seconds'
